ebe3cb30107dfb13486d10839dbde09eb65b1551
alexlied <alexlied@gmail.com>
2019-08-28 10:38:08 -0700

    Added signup fields

--- src/components/auth/Form.js
+++ src/components/auth/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { withRouter } from 'react-router'
 
 class Form extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       email: '',
@@ -13,44 +13,99 @@ class Form extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange ({ target: { name, value } }) {
+  handleChange({ target: { name, value } }) {
     this.setState({ [name]: value })
   }
 
-  handleSubmit (e) {
+  handleSubmit(e) {
     e.preventDefault()
     this.props.onSubmit(this.state)
       .then(() => this.props.history.push('/users'))
   }
 
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
-          <input
-            className='form-control'
-            id='email'
-            onChange={this.handleChange}
-            name='email'
-            type='text'
-            required
-            value={this.state.email} />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            className='form-control'
-            id='password'
-            onChange={this.handleChange}
-            name='password'
-            type='password'
-            required
-            value={this.state.password} />
-        </div>
-        <button type='submit' className='btn btn-primary'>Submit</button>
-      </form>
-    )
+  render() {
+    if (this.props.formType === 'signup') {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              className='form-control'
+              id='email'
+              onChange={this.handleChange}
+              name='email'
+              type='text'
+              required
+              value={this.state.email} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              className='form-control'
+              id='password'
+              onChange={this.handleChange}
+              name='password'
+              type='password'
+              required
+              value={this.state.password} />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='firstname'>First Name</label>
+            <input
+              className='form-control'
+              id='firstname'
+              onChange={this.handleChange}
+              name='firstname'
+              type='text'
+              required
+              value={this.state.firstname} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='lastname'>Last Name</label>
+            <input
+              className='form-control'
+              id='lastname'
+              onChange={this.handleChange}
+              name='lastname'
+              type='text'
+              required
+              value={this.state.lastname} />
+          </div>
+
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+      )
+    } else {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              className='form-control'
+              id='email'
+              onChange={this.handleChange}
+              name='email'
+              type='text'
+              required
+              value={this.state.email} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              className='form-control'
+              id='password'
+              onChange={this.handleChange}
+              name='password'
+              type='password'
+              required
+              value={this.state.password} />
+          </div>
+
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+      )
+    }
   }
 }
 
